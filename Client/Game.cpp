40576a3f0ad87000b09a2fa820e26c87d5c092ae
stopresent <mamy4938@gmai.com>
2022-10-09 22:38:00 +0900

    Vertex + Index Buffer로 만든 사각형

--- Client/Game.cpp
+++ Client/Game.cpp
@@ -9,22 +9,30 @@ void Game::Init(const WindowInfo& info)
 {
 	GEngine->Init(info);
 
-	vector<Vertex> vec(6);
+	vector<Vertex> vec(4);
 	vec[0].pos = Vec3(-0.5f, 0.5f, 0.5f);
 	vec[0].color = Vec4(1.f, 0.f, 0.f, 1.f);
 	vec[1].pos = Vec3(0.5f, 0.5f, 0.5f);
 	vec[1].color = Vec4(0.f, 1.f, 0.f, 1.f);
 	vec[2].pos = Vec3(0.5f, -0.5f, 0.5f);
 	vec[2].color = Vec4(0.f, 0.f, 1.f, 1.f);
+	vec[3].pos = Vec3(-0.5f, -0.5f, 0.5f);
+	vec[3].color = Vec4(0.f, 1.f, 0.f, 1.f);
 
-	vec[3].pos = Vec3(-0.5f, 0.5f, 0.5f);
-	vec[3].color = Vec4(1.f, 0.f, 0.f, 1.f);
-	vec[4].pos = Vec3(0.5f, -0.5f, 0.5f);
-	vec[4].color = Vec4(0.f, 0.f, 1.f, 1.f);
-	vec[5].pos = Vec3(-0.5f, -0.5f, 0.5f);
-	vec[5].color = Vec4(0.f, 1.f, 0.f, 1.f);
+	// 삼각형의 감기 순서는 시계 방향
+	vector<uint32> indexVec;
+	{
+		indexVec.push_back(0);
+		indexVec.push_back(1);
+		indexVec.push_back(2);
+	}
+	{
+		indexVec.push_back(3);
+		indexVec.push_back(0);
+		indexVec.push_back(2);
+	}
 
-	mesh->Init(vec);
+	mesh->Init(vec, indexVec);
 
 	shader->Init(L"..\\Resources\\Shader\\default.hlsli");
 
